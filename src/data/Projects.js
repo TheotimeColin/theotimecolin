e6215a9a22e55f8a8058fa95448d2751dec7bf0f
Théotime COLIN <contact@theotimecolin.com>
2019-09-03 11:03:07 +0200

    feat(Projects): add portfolio project and reorganize

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -2,6 +2,7 @@ import tempProjectMain0 from '@/assets/img/temp/project-main-0.png'
 import tempProjectMain1 from '@/assets/img/temp/project-main-1.png'
 import tempProjectMain2 from '@/assets/img/temp/project-main-2.png'
 import tempProjectMain3 from '@/assets/img/temp/project-main-3.png'
+import tempProjectMain4 from '@/assets/img/temp/project-main-4.png'
 
 import imageLeft0 from '@/assets/img/temp/project-left.jpg'
 import imageCenter0 from '@/assets/img/temp/project-center.jpg'
@@ -15,39 +16,6 @@ export default [
         image: tempProjectMain0,
         baseColor: '#ffe3bc',
         highlightColor: '#ffc551',
-        content: [
-            {
-                id: 0,
-                layout: 'image-left',
-                image: imageLeft0,
-                title: 'VueJs + ES6',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'
-            },
-            {
-                id: 1,
-                layout: 'text-column',
-                title: 'Une navigation dynamique',
-                image: null,
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'
-            },
-            {
-                id: 2,
-                layout: 'image-center',
-                image: imageCenter0,
-                bgColor: '#356dff',
-                textColor: '#ffffff',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'
-            },
-        ]
-    },
-    {
-        id: 1,
-        slug: 'superprof',
-        title: 'Superprof',
-        baseline: 'Trouvez le professeur parfait',
-        image: tempProjectMain2,
-        baseColor: '#fa6d8b',
-        highlightColor: '#ff2b51',
         content: [
             {
                 id: 0,
@@ -107,14 +75,47 @@ export default [
         ]
     },
     {
-        id: 2,
-        slug: 'weboucherie',
-        title: 'Weboucherie',
-        baseline: 'Une envie en un clic',
-        image: tempProjectMain3,
-        baseColor: '#ffcbcb',
-        highlightColor: '#fd7a9e',
+        id: 3,
+        slug: 'portfolio-2019',
+        title: 'Portfolio 2019',
+        baseline: 'code with passion, design with purpose, work with creativity',
+        image: tempProjectMain4,
+        baseColor: '#ffeb7d',
+        highlightColor: '#ff9968',
         content: [
+        ]
+    },
+    {
+        id: 1,
+        slug: 'superprof',
+        title: 'Superprof',
+        baseline: 'Trouvez le professeur parfait',
+        image: tempProjectMain2,
+        baseColor: '#fa6d8b',
+        highlightColor: '#ff2b51',
+        content: [
+            {
+                id: 0,
+                layout: 'image-left',
+                image: imageLeft0,
+                title: 'VueJs + ES6',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'
+            },
+            {
+                id: 1,
+                layout: 'text-column',
+                title: 'Une navigation dynamique',
+                image: null,
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'
+            },
+            {
+                id: 2,
+                layout: 'image-center',
+                image: imageCenter0,
+                bgColor: '#356dff',
+                textColor: '#ffffff',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'
+            },
         ]
     }
 ]
